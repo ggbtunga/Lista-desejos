e5dc3a52bcbd840185516548f9bfb50dcf897568
hick tan <hicktan@gmail.com>
2022-12-26 22:34:20 -0300

    itens no carousel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { useEffect, useState} from 'react';
+import { useEffect, useState,useRef} from 'react';
 import Nav from './components/Nav';
-import Body from './components/Body';
+// import Body from './components/Body';
 import './Page.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight,faCartShopping  } from "@fortawesome/free-solid-svg-icons";
 import products from './products/products';
 
 
 export default function App() {
 
-  const [figures,setFigures] = useState([])
+  const [figures,setFigures] = useState([]);
+  const carousel = useRef(null);
 
   useEffect(()=>{
     
@@ -15,11 +18,36 @@ export default function App() {
 
   },[]);
 
+  const handleLeftClick = (e)=>{
+    e.preventDefault()
+    carousel.current.scrollLeft-=carousel.current.offsetWidth
+  }
+  const handleRightClick = (e)=>{
+    e.preventDefault()
+    carousel.current.scrollLeft+=carousel.current.offsetWidth
+  }
+
   return (
 
     <div className="page">
       <Nav/>
-      <Body figures={figures}/>
+      <div className="container" ref={carousel}>
+            {figures.map(figure=>
+                <div className="cardItem" key={figure.id}>
+                    <img src={`assets/${figure.icon}.jpg`} 
+                        alt={figure.icon}>
+                    </img>
+                    <div className="cardItem-desc">
+                        <h1>{`Action Figure ${figure.icon}`}</h1>
+                        <FontAwesomeIcon icon={faCartShopping} className="figureCart"/>
+                    </div>
+                </div>
+            )}    
+      </div>
+      <div className="buttons">
+        <button onClick={handleLeftClick}><FontAwesomeIcon icon={faChevronLeft} /></button>
+        <button onClick={handleRightClick}><FontAwesomeIcon icon={faChevronRight} /></button>
+      </div>
     </div>
 
 
